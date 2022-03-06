362a23fc355e22871f4499cf4b58fe1fd74c0ad2
Савва Романов <savva.romanov99@gmail.com>
2022-03-06 14:58:20 +0300

    5

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -102,9 +102,10 @@ def profile_follow(request, username):
 
 @login_required
 def profile_unfollow(request, username):
-    get_object_or_404(
-        Follow,
-        user=request.user,
-        author__username=username
-    ).delete()
+    author = get_object_or_404(User, username=username)
+    follow = Follow.objects.filter(
+            user=request.user,
+            author=author)
+    if(follow.exists()):
+        follow.delete()
     return redirect('posts:profile', username=username)
